1b8c68e000881adc0497c7f7f6b121038c466613
Julian Edington <julianedington@gmail.com>
2021-11-09 19:50:39 -0600

    string to var

--- dev/update-modulemanifest.ps1
+++ dev/update-modulemanifest.ps1
@@ -3,7 +3,7 @@
 # $modulepath for Framework is: $env:USERPROFILE\Documents\WindowsPowerShell\Modules
 $modulepath = $env:PSModulePath.Split(';')[0]
 $source = "$modulepath\VOID-functions\VOID-functions.psd1"
-$manifest = Import-PowerShellDataFile "$modulepath\VOID-functions\VOID-functions.psd1"
+$manifest = Import-PowerShellDataFile $source
 [version]$Version = $manifest.ModuleVersion
 [version]$NewVersion = "{0}.{1}.{2}" -f $Version.Major, $Version.Minor, ($Version.Build + 1) 
 Update-ModuleManifest -Path $source -ModuleVersion $NewVersion
