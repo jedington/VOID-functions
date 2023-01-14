522dfbbbc5e1ad62838db5e28157644da2de1210
Julian Edington <julianedington@gmail.com>
2023-01-14 09:07:17 -0600

    add: Clear-Memory; fix: Start-ShredMultiple

---
+++ VOID-functions/functions/Clear-Memory.ps1
@@ -0,0 +1,24 @@
+<#
+	.SYNOPSIS
+	.DESCRIPTION
+	.NOTES
+#>
+
+#################################
+# CLEAR SYSTEM MEMORY (WINDOWS) #
+#################################
+function Clear-Memory {
+	if ([bool]($IsWindows) -or [bool]($PSVersionTable | Where-Object PSVersion -le 5 -EA 0)) {
+        Invoke-VOIDX "Memory used before cleanup: $([System.GC]::GetTotalMemory($false))"  
+        [System.GC]::Collect()
+        [System.GC]::WaitForPendingFinalizers()
+        [System.GC]::Collect()
+        Invoke-VOIDX "Memory used after cleanup: $([System.GC]::GetTotalMemory($true))"
+    }
+	elseif ([bool]($answeryn) -and ([bool]($IsLinux) -or [bool]($IsMacOS))) {
+		Invoke-VOIDX 'WINDOWS ONLY...'
+	}
+    else {
+        Invoke-VOIDX 'OPERATION CANCELLED'
+    }
+}
