8404f20e794df0868989ecc0de7db8c9aca961c2
Julian Edington <julianedington@gmail.com>
2025-05-16 07:59:19 -0500

    various fixes and subjective adjustments

--- VOID-functions/functions/Clear-BrowserCache.ps1
+++ VOID-functions/functions/Clear-BrowserCache.ps1
@@ -10,50 +10,41 @@
 function Clear-BrowserCache {
 	Invoke-VOIDT 'Will clear all browser cache.,.'
 	$answeryn = Invoke-VOIDYN 'Works on Windows only. Continue?'
+	
 	if ([bool]($answeryn) -and ([bool]($IsWindows) -or [bool]($PSVersionTable | Where-Object PSVersion -le 5 -EA 0))) {
 		$users = Get-ChildItem "$env:HOMEDRIVE\Users" | Select-Object Name
 		$users = $users.Name
-		if ([bool](Get-Service googlechrome*)) {
+		if ([bool](Get-Service brave*)) {
 			foreach ($user in $users) {
-				$bchromed = "$env:HOMEDRIVE\Users\$user\AppData\Local\Google\Chrome\User Data\Default"
-				Remove-Item -path "$bchromed\Cache\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bchromed\Cache2\entries\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bchromed\Cookies\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bchromed\Media Cache" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bchromed\Cookies-Journal" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bchromed\JumpListIconsOld" -Recurse -Force -EA 0 -Verbose
-			}
-			$profiles = Get-ChildItem -Path $bchromep | Select-Object Name | Where-Object Name -Like "Profile*"
-        	foreach ($account in $profiles) {
-				$accountp = $account.Name
-				$bchromep = "$env:HOMEDRIVE\Users\$user\AppData\Local\Google\Chrome\User Data"
-				Remove-Item -path "$bchromep\$accountp\Cache\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bchromep\$accountp\Cache2\entries\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bchromep\$accountp\Cookies\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bchromep\$accountp\Media Cache" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bchromep\$accountp\Cookies-Journal" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bchromep\$accountp\JumpListIconsOld" -Recurse -Force -EA 0 -Verbose
+				$bbrave = "$env:HOMEDRIVE\Users\$user\AppData\Local\BraveSoftware\Brave-Browser\User Data"
+				Remove-Item -path "$bbrave\Default\Cache\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bbrave\Default\Cookies\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bbrave\Default\Cookies-Journal" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bbrave\Default\IndexedDB\https_*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bbrave\Default\JumpListIconsOld" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bbrave\Default\Media Cache" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bbrave\Greaselion\Temp\*" -Recurse -Force -EA 0 -Verbose
 			}
 		}
-		if ([bool](Get-Service mozilla*)) {
+		if ([bool](Get-Service googlechrome*)) {
 			foreach ($user in $users) {
-				$bff = "$env:HOMEDRIVE\Users\$user\AppData\Local\Mozilla\Firefox\Profiles"
-				$bffdefault = "$bff\*.default"
-				$bffdev = "$bff\*.dev-edition-default"
-				Remove-Item -path "$bffdefault\cache\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdefault\cache2\entries\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdefault\OfflineCache\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdefault\thumbnails\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdefault\cookies.sqlite" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdefault\webappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdefault\chromeappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdev\cache\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdev\cache2\entries\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdev\OfflineCache\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdev\thumbnails\*" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdev\cookies.sqlite" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdev\webappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
-				Remove-Item -path "$bffdev\chromeappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
+				$bchrome = "$env:HOMEDRIVE\Users\$user\AppData\Local\Google\Chrome\User Data"
+				Remove-Item -path "$bchrome\Default\Cache\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bchrome\Default\Cache2\entries\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bchrome\Default\Cookies\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bchrome\Default\Cookies-Journal" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bchrome\Default\JumpListIconsOld" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bchrome\Default\Media Cache" -Recurse -Force -EA 0 -Verbose
+				$profiles = Get-ChildItem -Path $bchrome | Select-Object Name | Where-Object Name -Like "Profile*"
+        		foreach ($acct in $profiles) {
+					$acctp = $acct.Name
+					Remove-Item -path "$bchrome\$acctp\Cache\*" -Recurse -Force -EA 0 -Verbose
+					Remove-Item -path "$bchrome\$acctp\Cache2\entries\*" -Recurse -Force -EA 0 -Verbose
+					Remove-Item -path "$bchrome\$acctp\Cookies\*" -Recurse -Force -EA 0 -Verbose
+					Remove-Item -path "$bchrome\$acctp\Cookies-Journal" -Recurse -Force -EA 0 -Verbose
+					Remove-Item -path "$bchrome\$acctp\JumpListIconsOld" -Recurse -Force -EA 0 -Verbose
+					Remove-Item -path "$bchrome\$acctp\Media Cache" -Recurse -Force -EA 0 -Verbose
+				}
 			}
 		}
 		if ([bool](Get-Service microsoftedge*)) {
@@ -71,6 +62,52 @@ function Clear-BrowserCache {
 				Remove-Item -path "$bopera\Opera Stable\Cache\*" -Recurse -Force -EA 0 -Verbose
 			}
 		}
+		if ([bool](Get-Service mozilla*)) {
+			foreach ($user in $users) {
+				$bffL = "$env:HOMEDRIVE\Users\$user\AppData\Local\Mozilla\Firefox\Profiles"
+				$bffR = "$env:HOMEDRIVE\Users\$user\AppData\Roaming\Mozilla\Firefox\Profiles"
+				$bffdefL = "$bffL\*.default-release"
+				Remove-Item -path "$bffdefL\cache\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefL\cache2\entries\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefL\cache2\trash*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefL\OfflineCache\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefL\thumbnails\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefL\cookies.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefL\suggest.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefL\webappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefL\chromeappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
+				$bffdefR = "$bffR\*.default-release"
+				Remove-Item -path "$bffdefR\cache\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefR\cache2\entries\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefR\cache2\trash*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefR\OfflineCache\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefR\thumbnails\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefR\cookies.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefR\suggest.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefR\webappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdefR\chromeappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
+				$bffdevL = "$bffL\*.dev-edition-default"
+				Remove-Item -path "$bffdevL\cache\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevL\cache2\entries\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevL\cache2\trash*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevL\OfflineCache\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevL\thumbnails\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevL\cookies.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevL\suggest.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevL\webappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevL\chromeappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
+				$bffdevR = "$bffR\*.dev-edition-default"
+				Remove-Item -path "$bffdevR\cache\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevR\cache2\entries\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevR\cache2\trash*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevR\OfflineCache\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevR\thumbnails\*" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevR\cookies.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevR\suggest.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevR\webappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
+				Remove-Item -path "$bffdevR\chromeappsstore.sqlite" -Recurse -Force -EA 0 -Verbose
+			}
+		}
 	}
 	elseif ([bool]($IsLinux) -or [bool]($IsMacOS)) {
 		Invoke-VOIDX 'WINDOWS ONLY...'
@@ -81,14 +118,20 @@ function Clear-BrowserCache {
 	##############
 	# CLEAR VARS #
 	##############
-	Remove-Variable -Name 'accountp' -EA 0
+	Remove-Variable -Name 'acct' -EA 0
+	Remove-Variable -Name 'acctp' -EA 0
 	Remove-Variable -Name 'answeryn' -EA 0
-	Remove-Variable -Name 'bchromed' -EA 0
-	Remove-Variable -Name 'bchromep' -EA 0
-	Remove-Variable -Name 'bff' -EA 0
-	Remove-Variable -Name 'bffdefault' -EA 0
-	Remove-Variable -Name 'bffdev' -EA 0
+	Remove-Variable -Name 'bbrave' -EA 0
+	Remove-Variable -Name 'bchrome' -EA 0
 	Remove-Variable -Name 'bedge' -EA 0
 	Remove-Variable -Name 'bopera' -EA 0
+	Remove-Variable -Name 'bffL' -EA 0
+	Remove-Variable -Name 'bffR' -EA 0
+	Remove-Variable -Name 'bffdefL' -EA 0
+	Remove-Variable -Name 'bffdefR' -EA 0
+	Remove-Variable -Name 'bffdevL' -EA 0
+	Remove-Variable -Name 'bffdevR' -EA 0
+	Remove-Variable -Name 'profiles' -EA 0
+	Remove-Variable -Name 'user' -EA 0
 	Remove-Variable -Name 'users' -EA 0
 }
--- VOID-functions/functions/Clear-TempFiles.ps1
+++ VOID-functions/functions/Clear-TempFiles.ps1
@@ -15,26 +15,41 @@ function Clear-TempFiles {
 		$users = $users.Name
 		foreach ($user in $users) {
 			$userdir = "$env:HOMEDRIVE\Users\$user"
-			Remove-Item -path "$userdir\Dropbox\.dropbox.cache\*" -Recurse -Force -EA 0 -Verbose
-			Remove-Item -path "$userdir\Dropbox*\.dropbox.cache\*" -Recurse -Force -EA 0 -Verbose
-			$userappdata = "$userdir\AppData\Local"
-			Remove-Item -path "$userappdata\Temp\*" -Recurse -Force -EA 0 -Verbose
-			Remove-Item -path "$userappdata\CrashDumps\*" -Recurse -Force -EA 0 -Verbose
-			Remove-Item -path "$userappdata\Microsoft\Teams\previous\*" -Recurse -Force -EA 0 -Verbose
-			Remove-Item -path "$userappdata\Microsoft\Teams\stage\*" -Recurse -Force -EA 0 -Verbose
-			$userwindata = "$userappdata\Microsoft\Windows"
-			Remove-Item -path "$userwindata\Temporary Internet Files\*" -Recurse -Force -EA 0 -Verbose
-			Remove-Item -path "$userwindata\INetCache\* " -Recurse -Force -EA 0 -Verbose
-			Remove-Item -path "$userwindata\WebCache\* " -Recurse -Force -EA 0 -Verbose
-			Remove-Item -path "$userwindata\AppCache\*" -Recurse -Force -EA 0 -Verbose
-			Remove-Item -path "$userwindata\WER\*" -Recurse -Force -EA 0 -Verbose
+			$userappdata = "$userdir\AppData"
+			Remove-Item -path "$userappdata\Local\cache\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\CEF\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Comms\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\CrashDumps\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Diagnostics\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\ElevatedDiagnostics\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Microsoft\Feeds\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Microsoft\Teams\previous\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Microsoft\Teams\stage\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Microsoft\Windows\ActionCenterCache\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Microsoft\Windows\AppCache\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Microsoft\Windows\Explorer\thumbcache* " -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Microsoft\Windows\History\* " -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Microsoft\Windows\INetCache\* " -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Microsoft\Windows\WebCache\* " -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Microsoft\Windows\WER\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\npm-cache\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\nwjs\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Publishers\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\Temp\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Local\tyranoscript\*" -Recurse -Force -EA 0 -Verbose
+			Remove-Item -path "$userappdata\Roaming\Microsoft\Windows\Start Menu\Programs\*" -Recurse -Force -EA 0 -Verbose
 		}
-		Remove-Item -path "$env:windir\Temp\*" -Recurse -Force -EA 0 -Verbose
-		Remove-Item -path "$env:windir\Prefetch\*" -Recurse -Force -EA 0 -Verbose
-		Remove-Item -path "$env:windir\Logs\CBS\*" -Recurse -Force -EA 0 -Verbose
-		Remove-Item -path "$env:windir\SoftwareDistribution\*" -Recurse -Force -EA 0 -Verbose
-		Remove-Item -path "$env:windir\Downloaded Program Files\*" -Recurse -Force -EA 0 -Verbose
+		Remove-Item -path "$env:ProgramData\Microsoft\Network\Downloader\*" -Recurse -Force -EA 0 -Verbose
+		Remove-Item -path "$env:ProgramData\Microsoft\Search\Data\Applications\Windows\GatherLogs\SystemIndex\*" -Recurse -Force -EA 0 -Verbose
+		Remove-Item -path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\*.lnk" -Recurse -Force -EA 0 -Verbose
 		Remove-Item -path "$env:ProgramData\Microsoft\Windows\WER\*" -Recurse -Force -EA 0 -Verbose
+		Remove-Item -path "$env:ProgramData\Package Cache\*" -Recurse -Force -EA 0 -Verbose
+		Remove-Item -path "$env:ProgramData\USOShared\*" -Recurse -Force -EA 0 -Verbose
+		Remove-Item -path "$env:windir\Downloaded Program Files\*" -Recurse -Force -EA 0 -Verbose
+		Remove-Item -path "$env:windir\Prefetch\*" -Recurse -Force -EA 0 -Verbose
+		Remove-Item -path "$env:windir\SoftwareDistribution\*" -Recurse -Force -EA 0 -Verbose
+		Remove-Item -path "$env:windir\SystemTemp\*" -Recurse -Force -EA 0 -Verbose
+		Remove-Item -path "$env:windir\Temp\*" -Recurse -Force -EA 0 -Verbose
 	}
 	elseif ([bool]($IsLinux) -or [bool]($IsMacOS)) {
 		Invoke-VOIDX 'WINDOWS ONLY...'
@@ -48,6 +63,5 @@ function Clear-TempFiles {
 	Remove-Variable -Name 'answeryn' -EA 0
 	Remove-Variable -Name 'userdir' -EA 0
 	Remove-Variable -Name 'userappdata' -EA 0
-	Remove-Variable -Name 'userwindata' -EA 0
 	Remove-Variable -Name 'users' -EA 0
 }
